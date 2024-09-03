cf389fd53a86b904345632096ef6cbe925c3d7ad
Finley-Maple <yfy990824@163.com>
2024-09-03 10:30:24 +0200

    update training code; update the agent name

--- agent_code/Yu_policy_agent/config.py
+++ agent_code/Yu_policy_agent/config.py
@@ -1,14 +1,14 @@
 # Hyperparameters
 import os
 
-MODEL_NAME = 'coin' # Model name
+MODEL_NAME = 'Peaceful_agent' # Model name
 MODEL_TYPE = 'LSTM' # 'FF' or 'LSTM' or 'PPO'
-SEQ_LEN = 1 # Sequence length for LSTM
+SEQ_LEN = 2 # Sequence length for LSTM
 N_LAYERS = 3 # Number of layers for FF or LSTM
 LAST_EPISODE = 0 # Last episode number
 ALPHA = 1.0 # Weight for imitation learning loss
 
-FEATURE_DIM = 14 # 22 for full dimension, 18 excluded dead ends and 14 excluded crates
+FEATURE_DIM = 22 # 22 for full dimension, 18 excluded dead ends and 14 excluded crates
 
 # Path
 MODEL_PATH = os.path.join(os.path.dirname(__file__), 'checkpoints', MODEL_NAME + '_'+ 
